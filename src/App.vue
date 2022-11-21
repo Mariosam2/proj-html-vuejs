4c8d47221b3f0676673ae9136695ce00cec56bfa
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-21 14:59:26 +0100

    edited header comp, working on jumbo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <script>
+import AppHeader from './components/layout/AppHeader.vue';
+import AppMain from './components/layout/AppMain.vue';
+import AppFooter from './components/layout/AppFooter.vue';
 export default {
+  name: 'App',
+  components: {
+    AppHeader, AppMain, AppFooter
+  }
 
 }
 </script>
 
 <template>
-  <h1>Scaffholding</h1>
+  <!-- macroelements -->
+  <app-header></app-header>
+  <app-main></app-main>
+  <app-footer></app-footer>
+
 </template>
+
 
 <style>
 
---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,73 @@
+<script>
+export default {
+    name: 'Appheader',
+}
+</script>
+<template>
+    <header id="site_header" class="px-5">
+        <nav class="navbar navbar-expand-lg ms_nav px-5">
+            <div class="container-fluid ms_container">
+                <a class="navbar-brand ms_logo" href="#"><img src="../../assets/img/author-logo-round-small.png"
+                        alt="" /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item ms_nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item ms_nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item ms_nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item ms_nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item ms_nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item ms_nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item ms_nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item ms_nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+
+
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+        <!-- /.ms_nav -->
+        <!-- component jumbotron -->
+        <div class="container">
+            <div class="row align-items-md-stretch">
+                <div class="col-md-6">
+                    <div class="h-100 text-white rounded-3">
+                        <h2>Change the background</h2>
+                        <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron
+                            look.
+                            Then,
+                            mix and match with additional component themes and more.</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </header>
+    <!-- /#site_header -->
+</template>
+<style>
+
+</style>
