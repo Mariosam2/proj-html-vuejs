9501a8e183700ab8fca353eff771fd50f9aa2b74
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-21 17:24:35 +0100

    completed jumbotron header

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -5,10 +5,9 @@ export default {
 </script>
 <template>
     <header id="site_header" class="px-5">
-        <nav class="navbar navbar-expand-lg ms_nav px-5">
-            <div class="container-fluid ms_container">
-                <a class="navbar-brand ms_logo" href="#"><img src="../../assets/img/author-logo-round-small.png"
-                        alt="" /></a>
+        <div class="layover"></div>
+        <nav class="navbar navbar-expand-lg ms_nav pe-5">
+            <div class="container-fluid">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
@@ -49,15 +48,33 @@ export default {
         </nav>
         <!-- /.ms_nav -->
         <!-- component jumbotron -->
-        <div class="container">
+        <div class="ms_container-large">
             <div class="row align-items-md-stretch">
-                <div class="col-md-6">
-                    <div class="h-100 text-white rounded-3">
-                        <h2>Change the background</h2>
-                        <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron
-                            look.
-                            Then,
-                            mix and match with additional component themes and more.</p>
+                <div class="col-md-6 jumbo position-relative">
+                    <img src="../../assets/img/author-logo-round-small.png" alt="" class="ms_logo">
+                    <div class="h-100 text-white">
+                        <h2 class="jumbo-title">Damon Vaugh</h2>
+                        <p class="jumbo-header-p">Best-selling author and the most influential public intellectual in
+                            the western world right now</p>
+                        <span class="caption d-block">
+                            <i>- The New York Times</i>
+                        </span>
+                        <div class="next-prev d-flex mt-2">
+                            <div class="btn ms_btn-jumbo-header prev  my-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                                </svg>
+                            </div>
+                            <div class="btn ms_btn-jumbo-header next my-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                </svg>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
