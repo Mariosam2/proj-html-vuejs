09c160343f06e5cc381a072eafbcd8ea6219e587
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-22 14:40:35 +0100

    added jumbo dynamic and logos section

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,6 +1,11 @@
 <script>
+import NavbarItem from '../NavbarItem.vue';
+import Jumbotron from '../JumboItem.vue';
 export default {
     name: 'Appheader',
+    components: {
+        NavbarItem, Jumbotron
+    }
 }
 </script>
 <template>
@@ -16,80 +21,17 @@ export default {
                         class="smaller align-top">&dollar;</span>39</span>
                 On Sale</div>
         </div>
-        <nav class="navbar navbar-expand-lg ms_nav pe-5">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item ms_nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item ms_nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item ms_nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item ms_nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item ms_nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item ms_nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item ms_nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item ms_nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-
-
-                    </ul>
-
-                </div>
-            </div>
-        </nav>
-        <!-- /.ms_nav -->
+        <navbar-item></navbar-item>
         <!-- component jumbotron -->
         <div class="ms_container-large">
             <div class="row align-items-md-stretch">
-                <div class="col-md-6 jumbo position-relative">
+                <div class="col-md-6 position-relative">
                     <img src="../../assets/img/author-logo-round-small.png" alt="" class="ms_logo">
-                    <div class="h-100 text-white">
-                        <h2 class="jumbo-title font-script">Damon Vaugh</h2>
-                        <p class="jumbo-header-p font-pt-serif">Best-selling author and the most influential public
-                            intellectual in
-                            the western world right now</p>
-                        <span class="caption d-block">
-                            <i>- The New York Times</i>
-                        </span>
-                        <div class="next-prev d-flex mt-2">
-                            <div class="btn ms_btn-jumbo-header prev  my-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-arrow-left" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd"
-                                        d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
-                                </svg>
-                            </div>
-                            <div class="btn ms_btn-jumbo-header next my-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-arrow-right" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd"
-                                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-                                </svg>
-                            </div>
-                        </div>
-
-                    </div>
+                    <jumbotron :title="'Damon Vaugh'"
+                        :content="'Best - selling author and the most influential public intellectual in the western world right now'"
+                        :myKey="'header'"></jumbotron>
                 </div>
-                <div class="col-4 bg-white call-to-action">
+                <div class="col-4 d-none d-xl-block bg-white call-to-action">
                     <div class="flex-container d-flex">
                         <div class="flex-item">
                             <img src="../../assets/img/book-widget.png" alt="">
@@ -103,7 +45,7 @@ export default {
                             <div class="content">
                                 <h4 class="font-pt-serif">Latest Book Release</h4>
                                 <h6 class="text-uppercase text_primary pt-1">D. Vaughn Autobiography</h6>
-                                <p class="d-none d-xl-block caption pt-1"> Lorem ipsum dolor sit amet consectetur
+                                <p class="caption pt-1"> Lorem ipsum dolor sit amet consectetur
                                     adipisicing elit. Aut
                                     vel provident voluptatem incidunt excepturi! Nesciunt repellendus minus eveniet
                                     dolores provident.</p>
--- src/components/layout/AppMain.vue
+++ src/components/layout/AppMain.vue
@@ -1,8 +1,41 @@
 <script>
+import Jumbotron from '../JumboItem.vue'
 export default {
+    name: 'AppMain',
+    components: {
+        Jumbotron
+    }
 }
 </script>
 <template>
+    <main id="site_main">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center my-5">
+                    <jumbotron :title="'More About Damon Vaugh'" :content="`Damon Vaugh is a professor of psychology at the University of Toronto, a clinical psychologist and the author of the multi-million copy bestseller '
+                    The story of my life'`" :myKey="'main'"></jumbotron>
+                </div>
+            </div>
+        </div>
+        <section class="bg_dawn-pink py-5 mt-5">
+            <div class="container">
+                <div class="row row-cols-1 row-cols-xl-4 logos">
+                    <div class="col logo">
+                        <img src="../../assets/img/ebay-logotan-2x-600x197.png" alt="" />
+                    </div>
+                    <div class="col logo">
+                        <img src="../../assets/img/audible-logotan-2x-600x197.png" alt="" />
+                    </div>
+                    <div class="col logo">
+                        <img src="../../assets/img/bb-logotan-2x-600x197.png" alt="" />
+                    </div>
+                    <div class="col logo">
+                        <img src="../../assets/img/kindlefire-logotan-2x-600x197.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </template>
 <style>
 
